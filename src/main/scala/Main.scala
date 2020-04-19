0c31a213626b3157c4a4ca8b0568197db0c3c2ad
robertjankowski <robjanw11@wp.pl>
2020-04-19 22:07:13 +0200

    fix plotting script

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import graph.BarabasiAlbertSimulation
 object Main extends App {
   val sizes = List(100, 1000, 10000, 100000, 1000000)
   val ms = List(1, 2, 5)
-//   BarabasiAlbertSimulation.degreeDistributionExperiment(sizes, ms, "output/ba_degree")
+  //  BarabasiAlbertSimulation.degreeDistributionExperiment(sizes, ms, "output/ba_degree")
 
   val sizes2 = List(50, 100, 200, 500, 1000, 2000)
   BarabasiAlbertSimulation
