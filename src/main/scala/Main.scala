84ffe496824d7dc250655ded6f3072462fdc957d
robertjankowski <robjanw11@wp.pl>
2020-04-29 12:17:05 +0200

    generate more er graphs + measure time of ER simulation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,5 +2,5 @@ import graph.ErdosRenyiSimulation
 
 object Main extends App {
   ErdosRenyiSimulation
-    .compareTraditionalMethodWithMonteCarloSimulation(1000, 0.55, "output/p_55_n_1000.txt")
+    .compareTraditionalMethodWithMonteCarloSimulation(100, 0.80, "output/p_80_n_100.txt")
 }
